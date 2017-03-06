4367c0250eecd8690a18c00a6e2b0cd054a37522
Sascha Knöchel <knoechel@softbauware.de>
2017-03-06 16:56:12 +0100

    Migrate ios and material in one app

---
+++ www/js/batiming.js
@@ -0,0 +1,39 @@
+var isAndroid = Framework7.prototype.device.android === true;
+var isIos = Framework7.prototype.device.isIos === true;
+
+isAndroid = false;
+isIos = true;
+Template7.global = {
+    android: isAndroid,
+    ios: isIos
+}
+
+var $$ = Dom7;
+
+//add css styles
+if (isAndroid) {
+    $$('.view.navbar-through').removeClass('navbar-through').addClass('navbar-fixed');
+    // Insert content/elements, to the beginning of element specified in parameter
+    $$('.view .navbar').prependTo('.view .page');
+}
+
+// Initialize app
+var myApp = new Framework7({
+    material: isAndroid ? true : false,
+    template7Pages: true
+});
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, material design doesn't support it.
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function () {
+    console.log("Device is ready!");
+});
+
+myApp.onPageInit('about', function (page) {
+    console.log("enter about view");
+});
