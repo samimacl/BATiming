165562243570b0ff19cc8d4422f006e6f2d0dbfb
Sascha Knöchel <knoechel@softbauware.de>
2017-03-06 15:28:28 +0100

    added template Module, prepare for splitted views

---
+++ www/js/templateModule.js
@@ -0,0 +1,30 @@
+/* ----------------------------------------------------------------------- 
+*   templateModule.js - Template für Modul
+*
+*   (c) 2017 WS14-II - Tobias Groß, Sascha Knöchel, Andreas Garben, Atiq Butt
+*
+*  ----------------------------------------------------------------------- */
+
+var ba_main = (function () {
+    "use strict";
+
+    //Application object
+    var ba_main = {};
+
+    // ********************************************************************
+    //Private variables & private methods
+
+    var var1;
+
+    function _myfunc (param) {
+        // code here
+    }
+
+    // ********************************************************************
+    //Member variables & member methods
+
+    ba_main.attr1 = "";
+    ba_main.attr2 = {};
+
+    //init object
+})();
