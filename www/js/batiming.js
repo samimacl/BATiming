ac0652706a7799965a12b78a61b62fd68ec190ee
AGarben <andreas.garben@ba-rm.de>
2017-08-08 11:47:19 +0200

    WIP Index.js

--- www/js/batiming.js
+++ www/js/batiming.js
@@ -6,10 +6,18 @@ var isAndroid = Framework7.prototype.device.android === true;
 var isIos = Framework7.prototype.device.isIos === true;
 var userLoggedIn = false; //temporär für Testzwecke
 
+var config = {
+            apiKey: "AIzaSyANfxgws-hUd8UULyWBdUvz4BjRlBhq6e4",
+            authDomain: "ba-timing.firebaseapp.com",
+            databaseURL: "https://ba-timing.firebaseio.com",
+            storageBucket: "ba-timing.appspot.com",
+            messagingSenderId: "1050294194541"
+        };
+
 Template7.global = {
     android: isAndroid,
     ios: isIos
-}
+};
 
 var $$ = Dom7;
 
@@ -31,8 +39,9 @@ var myApp = new Framework7({
             view.router.loadPage('login.html');
 
             return false; //required to prevent default router action
-        }
-    }
+        } 
+    } 
+    
 });
 
 // Add view
@@ -44,6 +53,7 @@ var mainView = myApp.addView('.view-main', {
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function () {
     console.log("Device is ready!");
+    firebase.initializeApp(config);
 });
 
 myApp.onPageInit('login', function (page) {
@@ -51,23 +61,33 @@ myApp.onPageInit('login', function (page) {
         var email = $$('.login-screen input[name="username"]').val();
         var password = $$('.login-screen input[name="password"]').val();
 
-
-        //  var auth = firebase.auth();
-        // // sign up
-        // const promise = auth.createUserWithEmailAndPassword(email, password);
-        // promise.catch(e => console.log(e.message));
-
-
         firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
             if (errorCode === 'auth/wrong-password') {
-                alert('Wrong password.');
+                myApp.alert('Wrong password.');
             } else {
-                alert(errorMessage);
+                myApp.alert(errorMessage);
             }
             console.log(error);
         });
     });
 });
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+  userLoggedIn = true;
+  user.providerData.forEach(function (profile) {
+    console.log("Sign-in provider: "+profile.providerId);
+    console.log("  Provider-specific UID: "+profile.uid);
+    console.log("  Name: "+profile.displayName);
+    console.log("  Email: "+profile.email);
+    console.log("  Photo URL: "+profile.photoURL);
+    console.log("  UUID: "+user.uuid);
+  });
+
+  } else {
+   userLoggedIn = false;
+  }
+})
