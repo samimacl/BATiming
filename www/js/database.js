240bdc3f39b50f1213ba3fabaaef16f13e193329
sbw\tobias.gross <gross@softbauware.de>
2017-08-08 20:19:17 +0200

    Conflicts Database.js

--- www/js/database.js
+++ www/js/database.js
@@ -1,15 +1,11 @@
 /* ----------------------------------------------------------------------- 
-<<<<<<< HEAD
+*
 *   database.js - Funktionen für den Zugriff auf die Firebase-Datenbank.
-=======
-*   templateModule.js - Template für Modul
->>>>>>> 20a15bbf342ea9d97f4aef4db50af6c9ff982abb
 *
 *   (c) 2017 WS14-II - Tobias Groß, Sascha Knöchel, Andreas Garben, Atiq Butt
 *
 *  ----------------------------------------------------------------------- */
 
-<<<<<<< HEAD
 var Database = (function () {
     staticObject = null;
     this.fbInstance = null;
@@ -63,33 +59,3 @@ var Database = (function () {
 
 
 })();
-=======
-var ba_main = (function () {
-    "use strict";
-
-    //Application object
-    var ba_main = {};
-
-    // ********************************************************************
-    //Private variables & private methods
-
-    var var1;
-
-    function _myfunc(param) {
-        // code here
-    }
-
-    // ********************************************************************
-    //Member variables & member methods
-
-    ba_main.attr1 = "";
-    ba_main.attr2 = {};
-
-    //init object
-    ba_main.init = function () {
-        //code here
-    };
-
-    return ba_main;
-})();
->>>>>>> 20a15bbf342ea9d97f4aef4db50af6c9ff982abb
