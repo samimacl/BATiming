684e93c5589c7cdbb4c72444fee981aa99a6139b
AGarben <andreas.garben@ba-rm.de>
2017-08-10 13:25:44 +0200

    Settings Example

--- www/js/login.js
+++ www/js/login.js
@@ -1,38 +1,3 @@
-var batiming = batiming || {};
-
-batiming.Core = function () {};
-
-var isAndroid = Framework7.prototype.device.android === true;
-var isIos = Framework7.prototype.device.isIos === true;
-var devMode = true;
-
-Template7.global = {
-    android: isAndroid = false,
-    ios: isIos = true
-}
-
-var $$ = Dom7;
-
-//add css styles
-if (isAndroid) {
-    $$('.view.navbar-through').removeClass('navbar-through').addClass('navbar-fixed');
-    // Insert content/elements, to the beginning of element specified in parameter
-    $$('.view .navbar').prependTo('.view .page');
-}
-
-// Initialize app
-var myApp = new Framework7({
-    material: isAndroid === true ? true : false,
-    template7Pages: true,
-    swipePanel: 'left',
-    materialRipple: true
-});
-
-// Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, material design doesn't support it.
-    dynamicNavbar: true
-});
 
  $$('.login-screen .list-button').on('click', function () {
     firebase.auth().signInWithEmailAndPassword($$('.login-screen input[name = "username"]').val(), $$('.login-screen input[name = "password"]').val()).catch(function (error) {
@@ -74,14 +39,4 @@ firebase.auth().onAuthStateChanged(function (user) {
         }
         // No user is signed in.
     }
-});
-
-// Handle Cordova Device Ready Event
-$$(document).on('deviceready', function () {
-    console.log("Device is ready!");
-    beacon.initialize();
-
-    cordova.plugins.backgroundMode.enable();
-    if (isAndroid)
-        cordova.plugins.backgroundMode.overrideBackButton();
 });
