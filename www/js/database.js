5789d22a4b8b457ac42dc7320cdf1d68750fbd22
Sascha Knöchel <knoechel@softbauware.de>
2017-08-13 17:52:53 +0200

    Bug fixes for database, structure improved.

--- www/js/database.js
+++ www/js/database.js
@@ -1,195 +1,185 @@
 /* ----------------------------------------------------------------------- 
-*
-*   database.js - Funktionen für den Zugriff auf die Firebase-Datenbank.
-*
-*   (c) 2017 WS14-II - Tobias Groß, Sascha Knöchel, Andreas Garben, Atiq Butt
-*
-*  ----------------------------------------------------------------------- */
+ *
+ *   database.js - Funktionen für den Zugriff auf die Firebase-Datenbank.
+ *
+ *   (c) 2017 WS14-II - Tobias Groß, Sascha Knöchel, Andreas Garben, Atiq Butt
+ *
+ *  ----------------------------------------------------------------------- */
 
-var Database = function () {
-    var _singleton = null;
-    var fbInstance = firebase; //Setter/Getter ??
-    var fbUserMail = null;
-    var fbUserPassword = null;
-    var dbUrl = null;
+var Database = (function () {
+    let Database = {};
 
-    //Singleton-Pattern
-    return this.getInstance();
-};
+    let fbInstance = firebase; //Setter/Getter ??
+    let fbUserMail = null;
+    let fbUserPassword = null;
+    let dbUrl = null;
 
-Database.getInstance = function () {
-    if (_singleton == null) {
-        _singleton = new Database();
+    Database.createFirebaseObject = function (configParams, userMail, userPassword) {
+        fbInstance = firebase;
+        //  fbInstance.initializeApp(configParams); Ist bereits in Index, deshalb unnötig
+        this.setUserAuth(userMail, userPassword);
     }
-    return _singleton;
-}
 
-Database.createFirebaseObject = function(configParams, userMail, userPassword) {
-    fbInstance = firebase;
-    fbInstance.initializeApp(configParams);
-    this.setUserAuth(userMail, userPassword);
-}
-
-Database.getFirebaseObject = function () {
-    return fbInstance;
-}
-
-Database.setFirebaseObject = function (object) {
-    fbInstance = object;
-    //fbInstance.initializeApp({databaseUrl : dbUrl}); Muss im Login passieren
-}
-
-Database.setDatabaseUrl = function (url) {
-    dbUrl = url;
-}
-
-Database.setUserAuth = function (userMail, userPassword) {
-    fbUserMail = userMail;
-    fbUserPassword = userPassword;
-    fbInstance.auth().signInWithEmailAndPassword(userMail, userPassword);
-}
-
-//Returns String
-Database.getCurrentUserID = function () {
-    return fbInstance.auth().currentUser.uid;
-}
-
-//Returns JSON-Object
-Database.getCurrentPerson = function (callbackFunction) {
-    //Check if logged in
-    if (firebase.auth().currentUser) {
-        var ref = fbInstance.database().ref("Personen/Person_" + this.getCurrentUserID());
-        ref.once("value").then(function (snap) {
-            console.log("OnValue --> " + snap.val());
-            callbackFunction(snap.val());
-        });
-    } else {
-        //Login + Callback wenn eingeloggt, dann nochmaliger Funktionsaufruf
+    Database.getFirebaseObject = function () {
+        return fbInstance;
     }
-}
 
-//Returns JSON-Object
-Database.getPersonByID = function(userID, callbackFunction) {
-    //Check if logged in
-    if (firebase.auth().currentUser) {
-        if (!userID) {
-            var ref = fbInstance.database().ref("Personen/Person_" + userID);
+    Database.setFirebaseObject = function (object) {
+        fbInstance = object;
+        //fbInstance.initializeApp({databaseUrl : dbUrl}); Muss im Login passieren
+    }
+
+    Database.setDatabaseUrl = function (url) {
+        dbUrl = url;
+    }
+
+    Database.setUserAuth = function (userMail, userPassword) {
+        fbUserMail = userMail;
+        fbUserPassword = userPassword;
+        fbInstance.auth().signInWithEmailAndPassword(userMail, userPassword);
+    }
+
+    //Returns String
+    Database.getCurrentUserID = function () {
+        return fbInstance.auth().currentUser.uid;
+    }
+
+    //Returns JSON-Object
+    Database.getCurrentPerson = function (callbackFunction) {
+        //Check if logged in
+        if (firebase.auth().currentUser) {
+            var ref = fbInstance.database().ref("Personen/Person_" + this.getCurrentUserID());
             ref.once("value").then(function (snap) {
+                console.log("OnValue --> " + snap.val());
                 callbackFunction(snap.val());
             });
+        } else {
+            //Login + Callback wenn eingeloggt, dann nochmaliger Funktionsaufruf
         }
-        else {
-            callbackFunction(null);
-        }
-    } else {
-        //Login + Callback wenn eingeloggt, dann nochmaliger Funktionsaufruf
     }
-}
 
-//Returns void
-Database.createPerson = function (userID, name, vorname, studiengruppe, personalID, rolle) {
-     //Check if logged in
-    if (firebase.auth().currentUser) {
+    //Returns JSON-Object
+    Database.getPersonByID = function (userID, callbackFunction) {
+        //Check if logged in
+        if (firebase.auth().currentUser) {
+            if (!userID) {
+                var ref = fbInstance.database().ref("Personen/Person_" + userID);
+                ref.once("value").then(function (snap) {
+                    callbackFunction(snap.val());
+                });
+            } else {
+                callbackFunction(null);
+            }
+        } else {
+            //Login + Callback wenn eingeloggt, dann nochmaliger Funktionsaufruf
+        }
+    }
+
+    //Returns void
+    Database.createPerson = function (userID, name, vorname, studiengruppe, personalID, rolle) {
+        //Check if logged in
+        if (firebase.auth().currentUser) {
+            var ref = fbInstance.database().ref("Personen/Person_" + userID);
+            var newRef = ref.push();
+            newRef.set({
+                "Name": name,
+                "Vorname": vorname,
+                "Studiengruppe": studiengruppe,
+                "PersonalID": personalID,
+                "Rolle": rolle //0 = Student, 1 = Dozent
+            });
+        } else {
+            //Login + Callback wenn eingeloggt, dann nochmaliger Funktionsaufruf
+        }
+    }
+
+    //Returns void
+    Database.updatePerson = function (userID, name, vorname, studiengruppe, personalID) {
         var ref = fbInstance.database().ref("Personen/Person_" + userID);
-        var newRef = ref.push();
-        newRef.set({
-            "Name" : name,
-            "Vorname" : vorname,
-            "Studiengruppe" : studiengruppe,
-            "PersonalID" : personalID,
-            "Rolle" : rolle //0 = Student, 1 = Dozent
-        });
-    } else {
-        //Login + Callback wenn eingeloggt, dann nochmaliger Funktionsaufruf
-    }
-}
+        // var newRef = ref.push();
+        // newRef.set({
+        //     "Name" : name,
+        //     "Vorname" : vorname,
+        //     "Studiengruppe" : studiengruppe,
+        //     "PersonalID" : personalID,
+        // })
+    };
 
-//Returns void
-Database.updatePerson = function (userID, name, vorname, studiengruppe, personalID) {
-    var ref = fbInstance.database().ref("Personen/Person_" + userID);
-    // var newRef = ref.push();
-    // newRef.set({
-    //     "Name" : name,
-    //     "Vorname" : vorname,
-    //     "Studiengruppe" : studiengruppe,
-    //     "PersonalID" : personalID,
-    // })
-};
-
-//Returns String
-Database.getCurrentLectureKeyByStudyGroup = function(studyGroup, callbackFunction) {
-     //Check if logged in
-    if (firebase.auth().currentUser) {
-        if (!studyGroup) {
-            var date = Date();
-            var dateString = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
-            var timeString = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-            var ref = fbInstance.database().ref("StudyGroupCalendar/" + dateString + "/" + studyGroup);
-            ref.orderByKey().endAt(timeString).once("value").then(function (snap) {
-                // callbackFunction(snap.val());
-                snap.forEach(function(childNode){
-                    childNode.forEach(function(childChildNode) {
-                        var terminJSON = childChildNode.val();
-                        if (!terminJSON) {
-                            if (!terminJSON.Ende >= timeString) {
-                                callbackFunction(terminJSON.Vorlesung_ID);
+    //Returns String
+    Database.getCurrentLectureKeyByStudyGroup = function (studyGroup, callbackFunction) {
+        //Check if logged in
+        if (firebase.auth().currentUser) {
+            if (!studyGroup) {
+                var date = Date();
+                var dateString = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+                var timeString = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+                var ref = fbInstance.database().ref("StudyGroupCalendar/" + dateString + "/" + studyGroup);
+                ref.orderByKey().endAt(timeString).once("value").then(function (snap) {
+                    // callbackFunction(snap.val());
+                    snap.forEach(function (childNode) {
+                        childNode.forEach(function (childChildNode) {
+                            var terminJSON = childChildNode.val();
+                            if (!terminJSON) {
+                                if (!terminJSON.Ende >= timeString) {
+                                    callbackFunction(terminJSON.Vorlesung_ID);
+                                }
                             }
-                        }
+                        });
                     });
                 });
-            });
+            } else {
+                callbackFunction(null);
+            }
+        } else {
+            //Login + Callback wenn eingeloggt, dann nochmaliger Funktionsaufruf
         }
-        else
-        {
-            callbackFunction(null);
-        }
-    } else {
-        //Login + Callback wenn eingeloggt, dann nochmaliger Funktionsaufruf
     }
-}
 
-//Returns String-Array
-Database.getLectureKeysByStudyGroups = function(studyGroup, callbackFunction) {
-     //Check if logged in
-    if (firebase.auth().currentUser) {
-        if (!studyGroup) {
-            var ref = fbInstance.database().ref("StudyGroups/" + studyGroup + "/Lectures");
-            ref.once("value").then(function (snap) {
-                var lectureKeys = [];
-                snap.forEach(function(childNode) {
-                    lectureKeys.push(childNode.key);
+    //Returns String-Array
+    Database.getLectureKeysByStudyGroups = function (studyGroup, callbackFunction) {
+        //Check if logged in
+        if (firebase.auth().currentUser) {
+            if (!studyGroup) {
+                var ref = fbInstance.database().ref("StudyGroups/" + studyGroup + "/Lectures");
+                ref.once("value").then(function (snap) {
+                    var lectureKeys = [];
+                    snap.forEach(function (childNode) {
+                        lectureKeys.push(childNode.key);
+                    });
+                    callbackFunction(lectureKeys);
                 });
-                callbackFunction(lectureKeys);
-            });
+            } else {
+                callbackFunction(null);
+            }
         } else {
-            callbackFunction(null);
+            //Login + Callback wenn eingeloggt, dann nochmaliger Funktionsaufruf
         }
-    } else {
-        //Login + Callback wenn eingeloggt, dann nochmaliger Funktionsaufruf
     }
-}
 
-//Returns JSON-Object
-Database.getLectureByKey = function (lectureKey, callbackFunction) {
-     //Check if logged in
-    if (firebase.auth().currentUser) {
-        if (!studyGroup) {
-            var ref = fbInstance.database().ref("Lectures/" + lectureID);
-            ref.once("value").then(function (snap) {
-                callbackFunction(snap.val());
-            });
+    //Returns JSON-Object
+    Database.getLectureByKey = function (lectureKey, callbackFunction) {
+        //Check if logged in
+        if (firebase.auth().currentUser) {
+            if (!studyGroup) {
+                var ref = fbInstance.database().ref("Lectures/" + lectureID);
+                ref.once("value").then(function (snap) {
+                    callbackFunction(snap.val());
+                });
+            } else {
+                callbackFunction(null);
+            }
         } else {
-            callbackFunction(null);
+            //Login + Callback wenn eingeloggt, dann nochmaliger Funktionsaufruf
         }
-    } else {
-        //Login + Callback wenn eingeloggt, dann nochmaliger Funktionsaufruf
     }
-}
 
-Database.bookTimeEntry = function(terminID, excusedFlag, remark) {
-    return null;
-}
+    Database.bookTimeEntry = function (terminID, excusedFlag, remark) {
+        return null;
+    }
 
-Database.bookHistoryEntry = function(lectureDesc, terminID, roomDesc, bookingTime, remark) {
-    return null;
-}
+    Database.bookHistoryEntry = function (lectureDesc, terminID, roomDesc, bookingTime, remark) {
+        return null;
+    }
+
+    return Database;
+})();
