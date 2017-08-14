b0c6188311ac1842a3683f4ff81d79d0a1216878
AGarben <andreas.garben@ba-rm.de>
2017-08-14 17:00:05 +0200

    Deoppeltes entfernt

--- www/js/batiming.js
+++ www/js/batiming.js
@@ -40,13 +40,6 @@ var batiming = (function () {
         dynamicNavbar: true
     });
 
-    $$('.login-screen .register-login-screen').on('click', function () {
-        firebase.auth().createUserWithEmailAndPassword($$('.login-screen input[name = "username"]').val(), $$('.login-screen input[name = "password"]').val()).catch(function (error) {
-            myApp.alert(error.message);
-        });
-        myApp.closeModal('.login-screen');
-    });
-
     // Handle Cordova Device Ready Event
     $$(document).on('deviceready', function () {
         console.log("Device is ready!");
@@ -69,71 +62,6 @@ var batiming = (function () {
             .catch(function (e) {
                 console.log(e)
             });
-    });
-
-    $$('.sign-out').on('click', function () {
-        firebase.auth().signOut().then(function () {
-            // Sign-out successful.
-            myApp.loginScreen();
-        }).catch(function (error) {
-            // An error happened.
-            myApp.alert(error.message);
-        });
-    });
-
-    $$('.login-screen .list-button').on('click', function () {
-        firebase.auth().signInWithEmailAndPassword($$('.login-screen input[name = "username"]').val(), $$('.login-screen input[name = "password"]').val()).catch(function (error) {
-            myApp.alert(error.message);
-        });
-    });
-
-    $$('.login-screen .register-login-screen').on('click', function () {
-        firebase.auth().createUserWithEmailAndPassword($$('.login-screen input[name = "username"]').val(), $$('.login-screen input[name = "password"]').val()).catch(function (error) {
-            myApp.alert(error.message);
-        });
-    });
-
-    $$('.login-screen .resetpw-login-screen').on('click', function () {
-        firebase.auth().sendPasswordResetEmail($$('.login-screen input[name = "username"]').val()).then(function () {
-            myApp.alert("E-Mail versandt!");
-        }).catch(function (error) {
-            myApp.alert(error.message);
-        });
-    });
-
-    $$('.page .sign-out').on('click', function () {
-        firebase.auth().signOut().then(function () {
-            // Sign-out successful.
-            myApp.loginScreen();
-        }).catch(function (error) {
-            // An error happened.
-            myApp.alert(error.message);
-        });
-    });
-
-    firebase.auth().onAuthStateChanged(function (fbUser) {
-        if (fbUser) {
-            database.getCurrentPerson(function (data) {
-                console.log(data);
-                if (data == null)
-                    return;
-                if (data.Rolle != null && data.Rolle == '1') {
-                    $$('.view-main').hide();
-                    $$('.view-dozent').show();
-                } else {
-                    $$('.view-main').show();
-                    $$('.view-dozent').hide();
-                }
-                storageManager.addItem(true, 'userData', data);
-                myApp.closeModal('.login-screen');
-            });
-            console.log(fbUser);
-        } else {
-            storageManager.removeItem(true, 'userData');
-            if (devMode) {
-                myApp.closeModal('.login-screen');
-            }
-        }
     });
 
     // https://framework7.io/docs/form-storage.html
--- www/js/login.js
+++ www/js/login.js
@@ -2,6 +2,13 @@ $$('.login-screen .login-login-screen').on('click', function () {
     firebase.auth().signInWithEmailAndPassword($$('.login-screen input[name = "username"]').val(), $$('.login-screen input[name = "password"]').val()).catch(function (error) {
         myApp.alert(error.message);
     });
+    myApp.closeModal('.login-screen');
+});
+
+$$('.login-screen .list-button').on('click', function () {
+    firebase.auth().signInWithEmailAndPassword($$('.login-screen input[name = "username"]').val(), $$('.login-screen input[name = "password"]').val()).catch(function (error) {
+        myApp.alert(error.message);
+    });
 });
 
 $$('.login-screen .register-login-screen').on('click', function () {
@@ -9,13 +16,22 @@ $$('.login-screen .register-login-screen').on('click', function () {
         myApp.alert(error.message);
     });
     myApp.closeModal('.login-screen');
-
 });
 
 $$('.login-screen .resetpw-login-screen').on('click', function () {
     firebase.auth().sendPasswordResetEmail($$('.login-screen input[name = "username"]').val()).then(function () {
         myApp.alert("E-Mail versandt!");
     }).catch(function (error) {
+        myApp.alert(error.message);
+    });
+});
+
+$$('.page .sign-out').on('click', function () {
+    firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+        myApp.loginScreen();
+    }).catch(function (error) {
+        // An error happened.
         myApp.alert(error.message);
     });
 });
@@ -30,10 +46,12 @@ $$('.sign-out').on('click', function () {
     });
 });
 
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
+firebase.auth().onAuthStateChanged(function (fbUser) {
+    if (fbUser) {
         database.getCurrentPerson(function (data) {
             console.log(data);
+            if (data == null)
+                return;
             if (data.Rolle != null && data.Rolle == '1') {
                 $$('.view-main').hide();
                 $$('.view-dozent').show();
@@ -41,13 +59,14 @@ firebase.auth().onAuthStateChanged(function (user) {
                 $$('.view-main').show();
                 $$('.view-dozent').hide();
             }
+            storageManager.addItem(true, 'userData', data);
+            myApp.closeModal('.login-screen');
         });
-        myApp.closeModal('.login-screen');
-        console.log(user);
+        console.log(fbUser);
     } else {
+        storageManager.removeItem(true, 'userData');
         if (devMode) {
             myApp.closeModal('.login-screen');
         }
-        // No user is signed in.
     }
 });
