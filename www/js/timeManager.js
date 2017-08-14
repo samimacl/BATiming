1f8a3b6f1ebf1185da56a80b2ca3836bff799225
Sascha Knöchel <knoechel@softbauware.de>
2017-08-14 17:48:46 +0200

    start Workflow with cache and notification

--- www/js/timeManager.js
+++ www/js/timeManager.js
@@ -3,50 +3,56 @@ var timeManager = (function () {
 
     let state = 0;
 
-    function isDateInLecture(checkDate) {
-        let now = Date().now;
-        let date = Date(checkDate);
-        return date <= now;
+    function isDateInLecture(lectureDate) {
+        let now = new Date(Date.now());
+        return lectureDate.from <= now && lectureDate.end >= now;
     };
 
     timeManager.startWorkflow = async function () {
         if (state > 0)
             throw Error('Workflow already started.');
-        // check, whether localStorage for lesson is stored
-        // if not, load data through database object
-        let currentLecture = storageManager.getItem(true, 'currentLecture');
+        let currentLecture = JSON.parse(storageManager.getItem(true, 'currentLecture'));
         if (currentLecture === null) {
             let user = JSON.parse(storageManager.getItem(true, 'userData'));
             database.getCurrentLectureKeyByStudyGroup(user.Studiengruppe, function (data) {
                 console.log(data);
+                //check if data is null --> error
+                // storageManager.setItem(true, 'currentLecture', data); --> check data structure
             });
         } else {
-            // if (isDateInLecture(current))
+            if (isDateInLecture(currentLecture)) {
+                myApp.addNotification({
+                    "title": 'error',
+                    "message": 'Already signed in to lecture'
+                });
+                throw Error("Already signed in to lecture:" + "\n" + JSON.stringify(currentLecture));
+            }
         }
+        this.state++;
     }
 
     timeManager.bookTimeEntry = async function (pluginResult) {
-        if (state = 1) {
+        if (this.state = 1) {
             console.log("Begin saving time entry");
             //Database.Save....
             //make <p> visible with detail Information, button moves slower down with animation und got disabled
             // if succesfull:
-            state++;
+            this.state++;
             let bookedData = [];
             timeManager.writeHistoryEntry(bookedData);
         }
     }
 
     timeManager.writeHistoryEntry = async function (bookedData) {
-        if (state == 2) {
+        if (this.state == 2) {
             //write history entry  
-            state++;
+            this.state++;
             timeManager.stopWorkflow();
         }
     }
 
     timeManager.stopWorkflow = function () {
-        timeManager.state = 0;
+        this.state = 0;
         console.log('Workflow stopped...');
     }
 
