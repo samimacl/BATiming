e8d4808f12082b8c0b7b6d0cabbf77b2c4842b2c
AGarben <andreas.garben@ba-rm.de>
2017-08-15 15:27:33 +0200

    ...

--- www/js/batiming.js
+++ www/js/batiming.js
@@ -30,32 +30,35 @@ var batiming = (function () {
         swipePanel: 'left',
     });
 
-// https://framework7.io/docs/form-storage.html
-// https://framework7.io/docs/form-data.html
-myApp.onPageInit('settings', function (page) {
-    // Daten befüllen Example
-    var formData = {
-        'vorname': 'Andreas',
-        'nachname': 'Garben',
-        'email': 'john@doe.com',
-        'matrikelnummer': '12345',
-        'fachbereich': '2'
-    }
-    myApp.formFromData('#my-form', formData);
-});
+    // https://framework7.io/docs/form-storage.html
+    // https://framework7.io/docs/form-data.html
+    myApp.onPageInit('settings', function (page) {
+        // Daten befüllen Example
+        var formData = {
+            'vorname': 'Andreas',
+            'nachname': 'Garben',
+            'email': 'john@doe.com',
+            'matrikelnummer': '12345',
+            'fachbereich': '2'
+        }
+        myApp.formFromData('#my-form', formData);
+    });
 
-myApp.onPageBack('settings', function (page) {
-    // Daten Speichern beim Seite verlassen Example
-    var storedData = myApp.formGetData('my-form');
-    if (storedData) {
-        // Speeichern
-        myApp.alert(JSON.stringify(storedData));
-    } else {
-        // keine Änderungen
-        alert('Yet there is no stored data for this form. Please try to change any field')
-    }
-});
+    myApp.onPageBack('settings', function (page) {
+        // Daten Speichern beim Seite verlassen Example
+        var storedData = myApp.formGetData('my-form');
+        if (storedData) {
+            // Speeichern
+            myApp.alert(JSON.stringify(storedData));
+        } else {
+            // keine Änderungen
+            alert('Yet there is no stored data for this form. Please try to change any field')
+        }
+    });
 
- $$('.panel-close').on('click', function (e) {
-        myApp.closePanel();
-});
+    $$('.panel-close').on('click', function (e) {
+            myApp.closePanel();
+    });
+
+    return batiming;
+})();
