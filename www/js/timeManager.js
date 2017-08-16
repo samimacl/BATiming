ca848bf54b54816a2826957a81fa1979b8271ece
Sascha Knöchel <knoechel@softbauware.de>
2017-08-16 14:13:56 +0200

    Reorderded firebase config.

--- www/js/timeManager.js
+++ www/js/timeManager.js
@@ -25,7 +25,7 @@ var timeManager = (function () {
             });
             throw Error(message);
         }
-        
+
         let currentLecture = JSON.parse(storageManager.getItem(true, 'currentLecture'));
         if (currentLecture === null) {
             let user = JSON.parse(storageManager.getItem(true, 'userData'));
@@ -49,34 +49,33 @@ var timeManager = (function () {
             });
             throw Error("Already signed in to lecture:" + "\n" + JSON.stringify(currentLecture));
         }
-    }
-    state++;
-}
-
-timeManager.bookTimeEntry = async function (pluginResult) {
-    if (state = 1) {
-        console.log("Begin saving time entry");
-        //Database.Save....
-        //make <p> visible with detail Information, button moves slower down with animation und got disabled
-        // if succesfull:
         state++;
-        let bookedData = [];
-        timeManager.writeHistoryEntry(bookedData);
     }
-}
 
-timeManager.writeHistoryEntry = async function (bookedData) {
-    if (state == 2) {
-        //write history entry  
-        state++;
-        timeManager.stopWorkflow();
+    timeManager.bookTimeEntry = async function (pluginResult) {
+        if (state = 1) {
+            console.log("Begin saving time entry");
+            //Database.Save....
+            //make <p> visible with detail Information, button moves slower down with animation und got disabled
+            // if succesfull:
+            state++;
+            let bookedData = [];
+            timeManager.writeHistoryEntry(bookedData);
+        }
     }
-}
 
-timeManager.stopWorkflow = function () {
-    state = 0;
-    console.log('Workflow stopped...');
-}
+    timeManager.writeHistoryEntry = async function (bookedData) {
+        if (state == 2) {
+            //write history entry  
+            state++;
+            timeManager.stopWorkflow();
+        }
+    }
 
-return timeManager;
+    timeManager.stopWorkflow = function () {
+        state = 0;
+        console.log('Workflow stopped...');
+    }
+
+    return timeManager;
 })();
