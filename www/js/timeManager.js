eba3aa3ce6a134a9973a3838f0b93619d6b0b1a3
Sascha Knöchel <knoechel@softbauware.de>
2017-08-16 13:01:03 +0200

    WIP Notification in timeManager.

--- www/js/timeManager.js
+++ www/js/timeManager.js
@@ -17,52 +17,66 @@ var timeManager = (function () {
     };
 
     timeManager.startWorkflow = async function () {
-        if (state > 0)
-            throw Error('Workflow already started.');
+        if (state > 0) {
+            let message = 'Workflow already started.';
+            myApp.addNotification({
+                "title": 'Hint',
+                "message": message
+            });
+            throw Error(message);
+        }
+        
         let currentLecture = JSON.parse(storageManager.getItem(true, 'currentLecture'));
         if (currentLecture === null) {
             let user = JSON.parse(storageManager.getItem(true, 'userData'));
             database.getCurrentLectureKeyByStudyGroup(user.Studiengruppe, function (data) {
-                console.log(data);
-                //check if data is null --> error
-                // storageManager.setItem(true, 'currentLecture', data); --> check data structure
+                if (data == null) {
+                    let message = 'No data found';
+                    myApp.addNotification({
+                        "title": 'Error',
+                        "message": message
+                    });
+                    throw new Error(message);
+                }
+                storageManager.setItem(true, 'currentLecture', data);
             });
-        } else {
-            if (isDateInLecture(currentLecture)) {
-                myApp.addNotification({
-                    "title": 'error',
-                    "message": 'Already signed in to lecture'
-                });
-                throw Error("Already signed in to lecture:" + "\n" + JSON.stringify(currentLecture));
-            }
         }
-        this.state++;
-    }
 
-    timeManager.bookTimeEntry = async function (pluginResult) {
-        if (this.state = 1) {
-            console.log("Begin saving time entry");
-            //Database.Save....
-            //make <p> visible with detail Information, button moves slower down with animation und got disabled
-            // if succesfull:
-            this.state++;
-            let bookedData = [];
-            timeManager.writeHistoryEntry(bookedData);
+        if (isDateInLecture(currentLecture)) {
+            myApp.addNotification({
+                "title": 'error',
+                "message": 'Already signed in to lecture'
+            });
+            throw Error("Already signed in to lecture:" + "\n" + JSON.stringify(currentLecture));
         }
     }
+    state++;
+}
 
-    timeManager.writeHistoryEntry = async function (bookedData) {
-        if (this.state == 2) {
-            //write history entry  
-            this.state++;
-            timeManager.stopWorkflow();
-        }
+timeManager.bookTimeEntry = async function (pluginResult) {
+    if (state = 1) {
+        console.log("Begin saving time entry");
+        //Database.Save....
+        //make <p> visible with detail Information, button moves slower down with animation und got disabled
+        // if succesfull:
+        state++;
+        let bookedData = [];
+        timeManager.writeHistoryEntry(bookedData);
     }
+}
 
-    timeManager.stopWorkflow = function () {
-        this.state = 0;
-        console.log('Workflow stopped...');
+timeManager.writeHistoryEntry = async function (bookedData) {
+    if (state == 2) {
+        //write history entry  
+        state++;
+        timeManager.stopWorkflow();
     }
+}
 
-    return timeManager;
+timeManager.stopWorkflow = function () {
+    state = 0;
+    console.log('Workflow stopped...');
+}
+
+return timeManager;
 })();
