4401b2b365953a1fcb0938f5245e9cfa75701305
AGarben <andreas.garben@ba-rm.de>
2017-09-05 10:40:45 +0200

    BugFix

--- www/js/batiming.js
+++ www/js/batiming.js
@@ -213,23 +213,23 @@ var batiming = (function () {
             // Aktuelle Vorlesung
             database.getCurrentAppointmentByStudyGroup(JSON.parse(storageManager.getItem(true, 'userData')).Studiengruppe, function (data1) {
                 // Zukünftige Vorlesungen
-                database.getAppointmentList(5, JSON.parse(storageManager.getItem(true, 'userData')).Studiengruppe, function (data2) {
+                database.getAppointmentList(4, JSON.parse(storageManager.getItem(true, 'userData')).Studiengruppe, function (data2) {
                     // Letzte Einträge
                     database.getAppointmentList(-3, JSON.parse(storageManager.getItem(true, 'userData')).Studiengruppe, function (data3) {
 
                         if (data1 != null && data2 != null) {
                             if (data1[0] != undefined) {
                                 findAndRemove(data2, "appointment", data1[0].appointment)
-                                var searchedElement = data2.find(function (item) {
-                                    return item.date == data1[0].date;
-                                });
-                                if (searchedElement != null) {
-                                    data3.unshift(searchedElement);
-                                    delete data3[(data3.length - 1)];
-                                    data3.length = (data3.length - 1);
+                                // var searchedElement = data2.find(function (item) {
+                                //     return item.date == data1[0].date;
+                                // });
+                                // if (searchedElement != null) {
+                                //     data3.unshift(searchedElement);
+                                //     // delete data3[(data3.length - 1)];
+                                //     // data3.length = (data3.length - 1);
 
-                                    findAndRemove(data2, "date", data1[0].date)
-                                }
+                                //     findAndRemove(data3, "date", data1[0].date)
+                                // }
                             }
                         }
 
@@ -274,15 +274,6 @@ var batiming = (function () {
             if (result[property] === value) {
                 //Remove from array
                 array.splice(index, 1);
-            }
-        });
-    }
-
-    function findAndReturn(array, property, value) {
-        array.forEach(function (result, index) {
-            if (result[property] === value) {
-                //Remove from array
-                return array[index]
             }
         });
     }
